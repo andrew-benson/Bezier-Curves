3462371f297e751ccfb08c0c7849da9d3feb8ad6
andrew-benson <1992andybenson@gmail.com>
2020-06-08 01:28:28 +0100

    Unity Editor script extension for colouring selected objects

---
+++ Assets/Editor/ApplyColour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ApplyColour : EditorWindow
+{
+    private Color selectedColor;
+
+    [MenuItem("Window/Apply Object Colour")]
+    static void ShowWindow()
+    {
+        GetWindow<ApplyColour>().Show();
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Colour Picker", new GUIStyle { fontStyle = FontStyle.Bold });
+
+        selectedColor = EditorGUILayout.ColorField("Colour", selectedColor);
+
+        if (GUILayout.Button("Apply"))
+        {           
+            foreach (var obj in Selection.gameObjects)
+            {
+                var component = obj.GetComponent<Renderer>();
+                if(component)
+                {
+                    component.sharedMaterial.color = selectedColor;
+                }
+            }
+        }
+    }
+}
